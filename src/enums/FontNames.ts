17548bd6fed693c91c108061f51e704b31269267
Ian Paschal <projects@ianpaschal.com>
2021-06-04 08:48:24 +0200

    Create FontNames.ts

---
+++ src/enums/FontNames.ts
@@ -0,0 +1,24 @@
+export enum FontNames {
+	OPEN_SANS_BOLD = 'OpenSans-Bold',
+	OPEN_SANS_BOLD_ITALIC = 'OpenSans-Bold-Italic',
+	OPEN_SANS_EXTRA_BOLD = 'OpenSans-ExtraBold',
+	OPEN_SANS_EXTRA_BOLD_ITALIC = 'OpenSans-ExtraBold-Italic',
+	OPEN_SANS_LIGHT = 'OpenSans-Light',
+	OPEN_SANS_LIGHT_ITALIC = 'OpenSans-Light-Italic',
+	OPEN_SANS_REGULAR = 'OpenSans-Regular',
+	OPEN_SANS_REGULAR_ITALIC = 'OpenSans-Regular-Italic',
+	OPEN_SANS_SEMI_BOLD = 'OpenSans-SemiBold',
+	OPEN_SANS_SEMI_BOLD_ITALIC = 'OpenSans-SemiBold-Italic',
+	PT_SANS_REGULAR = 'PTSans-Regular',
+	PT_SANS_REGULAR_ITALIC = 'PTSans-Regular-Italic',
+	PT_SANS_BOLD = 'PTSans-Bold',
+	PT_SANS_BOLD_ITALIC = 'PTSans-Bold-Italic',
+}
+
+export enum FontWeights {
+	Light = 300,
+	Regular = 400,
+	SemiBold = 500,
+	Bold = 700,
+	ExtraBold = 800,
+}
