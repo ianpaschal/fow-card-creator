4d9300be47e3636414329bd1497530c9ea866ac4
Ian Paschal <i.paschal@ultimaker.com>
2021-07-08 22:34:13 +0200

    Add UnitCardTile component

---
+++ src/components/UnitCardTile/UnitCardTile.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { UnitCard } from '../../typing/UnitCard';
+import { computeCardRating } from '../../utils/computeCardRating';
+import { StarRating } from '../StarRating/StarRating';
+import './UnitCardTile.scss';
+
+export interface UnitCardTileProps {
+	card: UnitCard;
+	useFooter?: boolean;
+}
+
+export const UnitCardTile: React.FC<UnitCardTileProps> = ({
+	card,
+	useFooter = false,
+}: UnitCardTileProps) => {
+	const rating = computeCardRating(card.ratings);
+	return (
+		<Link className="unit-card-tile" to={`/edit/${card.id}`}>
+			<div className="unit-card-tile__image-section" style={{
+				backgroundColor: card.unit.accentColor,
+			}}>
+				<div className="unit-card-tile__image-wrapper">
+					{card.unit.primaryImageURL && (
+						<img src={card.unit.primaryImageURL} />
+					)}
+				</div>
+			</div>
+			<div className="unit-card-tile__header-section" style={{
+				backgroundColor: card.unit.accentColor,
+			}}>
+				<h2>{card.unit.title || 'Untitled'}</h2>
+				{card.unit.subTitle && (
+					<h3>{card.unit.subTitle}</h3>
+				)}
+			</div>
+			{useFooter && (
+				<div className="unit-card-tile__footer-section">
+					<StarRating rating={rating || 5} />
+				</div>
+			)}
+		</Link>
+	);
+};
